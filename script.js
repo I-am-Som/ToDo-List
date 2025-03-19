f3602185762e9c617ff17d3a9fa66a7665bd5ea1
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-03-19 15:03:11 +0530

    ...

--- script.js
+++ script.js
@@ -4,26 +4,26 @@ let toDoItem = function(toDoEvent) {
     counter++;
     return `
         <div class="toDoItem" id="item${counter}">
+                <input type="checkbox" class="taskCheck" id="check${counter}">
                 <span class="toDoText" id="text${counter}">${toDoEvent}</span>
-                <button class="delete-btn" id="delete${counter}">🗑️</button>
+                <button class="delete-btn" id="delete${counter}">❌</button>
             </div>
     `;
 }
 
 
-
 const addButton = document.querySelector(".addButton");
 addButton.addEventListener('click',
     ()=> {
-        console.log("event listener");
+        // console.log("event listener");
         let input = document.querySelector(".toDoInput").value;
         
         
-        if (input.trim() !== "") { // Prevent adding empty tasks
-            const result = toDoItem(input); // Get HTML string
-            console.log(result);
-            document.querySelector(".toDoList").insertAdjacentHTML("beforeend", result); // Convert string to HTML
-            document.querySelector(".toDoInput").value = ""; // Clear input
+        if (input.trim() !== "") { 
+            const result = toDoItem(input);
+            // console.log(result);
+            document.querySelector(".toDoList").insertAdjacentHTML("beforeend", result);
+            document.querySelector(".toDoInput").value = "";
         }
     }
 );
@@ -36,5 +36,24 @@ addButton.addEventListener('click',
                 const item = document.getElementById(toBeDeletedItem);
                 item.remove();
             }
+
+            if (targetInfo.getAttribute("class") === "taskCheck") {
+                const parentId = targetInfo.parentNode.getAttribute("id");
+                const parentElement = document.getElementById(parentId);
+                const toDoTextElement = parentElement.querySelector(".toDoText");
+            
+                
+                toDoTextElement.innerHTML = `<s>${toDoTextElement.innerText}</s>`;
+
+                setTimeout(
+                    () =>{
+                        const toBeDeletedItem = targetInfo.parentNode.getAttribute("id");
+                        const item = document.getElementById(toBeDeletedItem);
+                        item.remove();
+                    },800
+                )
+            }
+            
+            
         }
     );
